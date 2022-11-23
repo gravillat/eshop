bd6fc26064a39267391a0e5dd2852d08ccbfcadb
Mairam <anarbaevamayram@gmail.com>
2022-11-23 11:11:21 +0600

    Adding changes file

--- eshop/settings.py
+++ eshop/settings.py
@@ -126,8 +126,9 @@ AUTH_USER_MODEL = 'account.MyUser'
 MEDIA_URL = 'mediafiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, '')
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
